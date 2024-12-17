8c8e1ad7fb6b7d244a49f0c4830946829eeacfb2
Steeapy <lukischnittker@gmail.com>
2024-12-17 12:44:16 +0100

    WIP 2015 day1 part2

--- aoc-2015/day01/src/main.rs
+++ aoc-2015/day01/src/main.rs
@@ -1,13 +1,23 @@
 use std::fs;
-
+use std::str::Chars;
 
 fn main() {
     let input: String = fs::read_to_string("input/input.txt")
         .expect("Error reading input.txt");
-
     let input_chars = input.chars();
-    let mut floor: i32 = 0;
 
+    // Part1
+    let floor: i32 = find_final_position(input_chars);
+    println!("Part1 {}", floor);
+
+
+    // Part 2
+
+}
+
+//For Part1
+fn find_final_position(input_chars: Chars) -> i32{
+    let mut floor: i32 = 0;
     for ch in input_chars {
         if ch == '(' {
             floor += 1;
@@ -16,5 +26,19 @@ fn main() {
             floor -= 1;
         }
     }
-    println!("Part1 {}", floor);
+    floor
+}
+
+//For Part2
+fn find_first_basement_position(input_chars: Chars) -> i32 {
+    let mut floor: i32 = 0;
+    for (index ,ch) in input_chars.into_iter() { //TODO RESUME HERE
+        if ch == '(' {
+            floor += 1;
+        }
+        if ch == ')' {
+            floor -= 1;
+        }
+    }
+    floor
 }
