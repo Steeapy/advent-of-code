0edf09c539d030616f5b214157660e9b8ee7f85b
Steeapy <lukischnittker@gmail.com>
2024-12-17 12:18:07 +0100

    Add 2015 day1 part1

--- aoc-2015/day01/src/main.rs
+++ aoc-2015/day01/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    let input: String = fs::read_to_string("input/input.txt")
+        .expect("Error reading input.txt");
+
+    let input_chars = input.chars();
+    let mut floor: i32 = 0;
+
+    for ch in input_chars {
+        if ch == '(' {
+            floor += 1;
+        }
+        if ch == ')' {
+            floor -= 1;
+        }
+    }
+    println!("Part1 {}", floor);
 }
