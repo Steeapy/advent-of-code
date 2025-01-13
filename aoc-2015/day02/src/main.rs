2298a3bec86e4a13a03c3d1a0628265955a5d61c
Steeapy <144958950+Steeapy@users.noreply.github.com>
2025-01-13 13:54:56 +0100

    WIP 2015 day2 part1

--- aoc-2015/day02/src/main.rs
+++ aoc-2015/day02/src/main.rs
@@ -1,5 +1,37 @@
 use std::fs;
 
+
+struct Box{
+    length: u32,
+    height: u32,
+    width: u32,
+}
+
 fn main() {
-    
+    let input: String = fs::read_to_string("input/input.txt")
+        .expect("Error reading input.txt");
+    let mut dimensions: Vec<Box> = Vec::new();
+    let mut sum_sq_ft: u32 = 0;
+
+    for line in input.lines().into_iter() {
+        let values: Vec<&str> = line
+            .split('x').collect();
+
+        dimensions.push(Box{
+            length: values[0].parse().unwrap(),
+            height: values[1].parse().unwrap(),
+            width: values[2].parse().unwrap(),
+        });
+    }
+
+    for cube in dimensions{
+        let side1: u32 = 2*cube.length + 2*cube.width;
+        let side2: u32 = 2*cube.width + 2*cube.height;
+        let side3: u32 = 2*cube.height + 2*cube.length;
+        let smallest_side: u32 = (side1.min(side2).min(side3)) / 2;
+
+        sum_sq_ft += side1 + side2 + side3 + smallest_side;
+    }
+
+    println!("Part 1: {}", sum_sq_ft);
 }
