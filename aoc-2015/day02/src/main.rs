4023b944110d314b3f169904a695c043a61d0fe0
Steeapy <144958950+Steeapy@users.noreply.github.com>
2025-01-23 16:45:14 +0100

    WIP 2015 day2 part1

--- aoc-2015/day02/src/main.rs
+++ aoc-2015/day02/src/main.rs
@@ -10,9 +10,8 @@ struct Box{
 fn main() {
     let input: String = fs::read_to_string("input/input.txt")
         .expect("Error reading input.txt");
-    let mut dimensions: Vec<Box> = Vec::new();
-    let mut sum_sq_ft: u32 = 0;
 
+    let mut dimensions: Vec<Box> = Vec::new();
     for line in input.lines().into_iter() {
         let values: Vec<&str> = line
             .split('x').collect();
@@ -24,14 +23,27 @@ fn main() {
         });
     }
 
+    let mut sum_sq_ft: u32 = 0;
+    let mut sum_slack: u32 = 0;
     for cube in dimensions{
-        let side1: u32 = 2*cube.length + 2*cube.width;
-        let side2: u32 = 2*cube.width + 2*cube.height;
-        let side3: u32 = 2*cube.height + 2*cube.length;
+        let side1: u32 = 2*cube.length*cube.width;
+        let side2: u32 = 2*cube.width*cube.height;
+        let side3: u32 = 2*cube.height*cube.length;
         let smallest_side: u32 = (side1.min(side2).min(side3)) / 2;
+        sum_slack += smallest_side;
+
+        print!("Side1:{side1}\tSide2:{side2}\tSide3:{side3}\tsmallest:{smallest_side}");
+        let length: u32 = cube.length;
+        let height: u32 = cube.height;
+        let width: u32 = cube.width;
+
+        let total_box: u32 = side1 + side2 + side3 + smallest_side;
+        println!("\tLength:{length}\tHeight:{height}\tWidth:{width}\tTotalBox:{total_box}");
+
 
         sum_sq_ft += side1 + side2 + side3 + smallest_side;
     }
 
-    println!("Part 1: {}", sum_sq_ft);
+    println!("Part 1    : {}", sum_sq_ft);
+    println!("Part 1Slack: {}", sum_slack);
 }
